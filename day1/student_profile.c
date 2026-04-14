d9709164d03c1b1f8f347dcec7a5f677bd66782a
andaniika <an304@bgt.school>
2026-04-14 11:39:11 +0200

    Add student profile display in student_profile.c

---
+++ day1/student_profile.c
@@ -0,0 +1,9 @@
+int main() {
+    printf("===== Student Profile =====\n");
+    printf("Emri: Anda\n");
+    printf("Mbiemri: Nika\n");
+    printf("Shkolla: BGT\n");
+    printf("Grupi: 1\n");
+    printf("Drejtimi: Programim\n");
+    return 0;
+}

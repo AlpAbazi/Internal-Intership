6580104d1c8d1feb338acb38b34727f2ca0fa62e
aandixhemili-art <aandixhemili@gmail.com>
2026-04-14 11:40:16 +0200

    Add student profile display in Student_Profile.C

--- day1/Student_Profile.C
+++ day1/Student_Profile.C
@@ -1,1 +1,9 @@
+int main() {
+    printf("Student Profile");
+    printf("Name: Andi Xhemili");
+    printf("Class: 11");
+    printf("School: British Gymnasium of Technology");
+    printf("Favorite Language: C");
 
+    return 0;
+}

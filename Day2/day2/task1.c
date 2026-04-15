c913d524b6babb0ec606d9ceaa8b93a0a860e3dd
ALMIRI123465789 <am206@bgt.school>
2026-04-15 09:57:12 +0200

    Add user input and output for personal profile

--- Day2/day2/task1.c
+++ Day2/day2/task1.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
 
+int main() {
+    int age;
+    char initial;
+    float height;
+    double average;
+    short smallNumber = 10;
+    long bigNumber = 100000;
+
+    // Input
+    printf("Shkruaj moshen: ");
+    scanf("%d", &age);
+
+    printf("Shkruaj inicialin: ");
+    scanf(" %c", &initial);
+
+    printf("Shkruaj gjatesine (p.sh 1.75): ");
+    scanf("%f", &height);
+
+    printf("Shkruaj mesataren: ");
+    scanf("%lf", &average);
+
+    // Output
+    printf("\n--- PROFILI ---\n");
+    printf("Mosha: %d\n", age);
+    printf("Iniciali: %c\n", initial);
+    printf("Gjatesia: %.2f\n", height);
+    printf("Mesatarja: %.2lf\n", average);
+    printf("Short number: %d\n", smallNumber);
+    printf("Long number: %ld\n", bigNumber);
+
+    return 0;
+}

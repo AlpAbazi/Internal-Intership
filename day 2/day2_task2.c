a90bc808354245d38c9299a00c7aa8d1f7db2436
andaniika <an304@bgt.school>
2026-04-15 11:21:39 +0200

    Add grading program based on input score

---
+++ day 2/day2_task2.c
@@ -0,0 +1,24 @@
+int main() {
+    int rezultati;
+
+    printf("Shkruani rezultatin (0 - 100): ");
+    scanf("%d", &rezultati);
+
+    if (rezultati < 0 || rezultati > 100) {
+        printf("Input-i nuk eshte valid.\n");
+    } 
+    else if (rezultati >= 90) {
+        printf("Vleresimi: Shkelqyeshem\n");
+    } 
+    else if (rezultati >= 75) {
+        printf("Vleresimi: Shume mire\n");
+    } 
+    else if (rezultati >= 50) {
+        printf("Vleresimi: Mire\n");
+    } 
+    else {
+        printf("Vleresimi: Ne permiresim\n");
+    }
+
+    return 0;
+}

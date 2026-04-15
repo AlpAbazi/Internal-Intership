a8a4e03f25a5ea1b20956b3979b768c57867bb5c
AlpAbazi <aa208@bgt.school>
2026-04-15 09:55:00 +0200

    Update task1.c

--- day2/task1.c
+++ day2/task1.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+int main() {
+    int age;
+    char initial;
+    float height;
+    double average;
+    short favoriteNumber = 12;
+    long population = 1800000;
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("Enter your initial: ");
+    scanf(" %c", &initial);
+
+    printf("Enter your height: ");
+    scanf("%f", &height);
+
+    printf("Enter your average grade: ");
+    scanf("%lf", &average);
+
+    printf("\n--- User Profile ---\n");
+    printf("Age: %d\n", age);
+    printf("Initial: %c\n", initial);
+    printf("Height: %.2f\n", height);
+    printf("Average Grade: %.2lf\n", average);
+    printf("Favorite Number (short): %hd\n", favoriteNumber);
+    printf("Population (long): %ld\n", population);
+
+    return 0;
+}

6906a7aa6c67d0d7f622fcf117629494b469aa8d
AjlaKrasniqii <ak203@bgt.school>
2026-04-15 09:57:07 +0200

    Update day2_task1.c

--- day 2/day2_task1.c
+++ day 2/day2_task1.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+int main() {
+    int age;
+    char initial;
+    float height;
+    double average;
+
+    short year = 2025;
+    long population = 1800000;
+
+    printf("Enter age: ");
+    scanf("%d", &age);
+
+    printf("Enter initial: ");
+    scanf(" %c", &initial);
+
+    printf("Enter height: ");
+    scanf("%f", &height);
+
+    printf("Enter average: ");
+    scanf("%lf", &average);
+
+    printf("\n--- USER PROFILE ---\n");
+    printf("Age: %d\n", age);
+    printf("Initial: %c\n", initial);
+    printf("Height: %.2f\n", height);
+    printf("Average: %.2lf\n", average);
+    printf("Year: %d\n", year);
+    printf("Population: %ld\n", population);
+
+    return 0;
+}

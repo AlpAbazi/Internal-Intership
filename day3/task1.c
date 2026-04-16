61c019d5b0be7a0d882a0c4ee2afd2310f8180b3
ALMIRI123465789 <am206@bgt.school>
2026-04-16 10:55:52 +0200

    Update task1.c

--- day3/task1.c
+++ day3/task1.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
 
+int main() {
+    int n, choice;
+
+    // Input
+    printf("Shkruaj madhesine (1-12): ");
+    scanf("%d", &n);
+
+    // Kontroll validiteti
+    if(n < 1 || n > 12) {
+        printf("Input nuk eshte valid!\n");
+        return 0;
+    }
+
+    printf("Zgjedh pattern (1 = yje, 2 = numra): ");
+    scanf("%d", &choice);
+
+    // Pattern me yje
+    if(choice == 1) {
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                printf("* ");
+            }
+            printf("\n");
+        }
+    }
+    // Pattern me numra
+    else if(choice == 2) {
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                printf("%d ", j);
+            }
+            printf("\n");
+        }
+    }
+    else {
+        printf("Zgjedhje e pavlefshme!\n");
+    }
+
+    return 0;
+}

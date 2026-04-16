744c1e1f5873a82c03dc727cebb9a1bfd6e57187
AlpAbazi <aa208@bgt.school>
2026-04-16 10:57:18 +0200

    Update task2.c

--- day3/task2.c
+++ day3/task2.c
@@ -1,1 +1,47 @@
+#include <stdio.h>
 
+int main() {
+    int start, end;
+    int evenCount = 0, oddCount = 0;
+    int evenSum = 0, oddSum = 0;
+
+    printf("Shkruaj vleren fillestare: ");
+    scanf("%d", &start);
+
+    printf("Shkruaj vleren perfundimtare: ");
+    scanf("%d", &end);
+
+    if(start > end) {
+        printf("Interval jo valid!\n");
+        return 0;
+    }
+
+    for(int i = start; i <= end; i++) {
+        if(i % 2 == 0) {
+            evenCount++;
+            evenSum += i;
+        } else {
+            oddCount++;
+            oddSum += i;
+        }
+    }
+
+    printf("\nIntervali: %d - %d\n", start, end);
+    printf("Totali i numrave: %d\n", end - start + 1);
+
+    printf("Numra çift: %d\n", evenCount);
+    printf("Shuma e numrave çift: %d\n", evenSum);
+
+    printf("Numra tek: %d\n", oddCount);
+    printf("Shuma e numrave tek: %d\n", oddSum);
+
+    if(evenCount > oddCount) {
+        printf("Ka me shume numra çift.\n");
+    } else if(oddCount > evenCount) {
+        printf("Ka me shume numra tek.\n");
+    } else {
+        printf("Numrat çift dhe tek jane te barabarte.\n");
+    }
+
+    return 0;
+}
